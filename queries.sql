bb623aa1cb086922fc3f97b51c490e25989c067c
bettercalllelel <60786916+bettercalllelel@users.noreply.github.com>
2023-01-15 19:32:33 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,11 +6,13 @@ from `bdlelel.tabelalelel`
 order by transaction_date;
 
 --Query 2
+
 select
  round(avg(transaction_amount), 2) media_t
 from `bdlelel.tabelalelel`;
 
 --Query 3
+
 select
 date_trunc(transaction_date, week) semana
 , round(avg(transaction_amount), 2) media_semanal
@@ -19,6 +21,7 @@ group by 1
 order by 1 asc;
 
 --Query 4
+
 select
  user_id
  , device_id
@@ -30,6 +33,7 @@ group by 1, 2
 order by 3 desc;
 
 --Query 5
+
 select
  user_id
  , count(*) contagem_transacoes
@@ -40,6 +44,7 @@ group by 1
 order by 2 desc;
 
 --Query 6
+
 select
  transaction_id
  , user_id
@@ -50,6 +55,7 @@ where user_id = 78262 and has_cbk is true
 order by 3 desc;
 
 --Query 7
+
 select
  user_id,
  count(distinct card_number) contagem_cartoes
@@ -58,6 +64,7 @@ group by 1
 order by 2 desc;
 
 --Query 8
+
 select
  date_trunc(transaction_date, hour) hora
  , user_id
